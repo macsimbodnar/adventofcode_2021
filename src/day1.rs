8abba422007b90b22b3e3bcb2daa374f1900effc
Maksym Bodnar <macsimbodnar@gmail.com>
2021-12-07 14:48:29 +0100

    day1 part two

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,16 @@
 use crate::utils::read;
 
-pub fn count_increases() {
-  let measures = read("assets/day1/measurements.txt");
+const SAMPLES: &str = "assets/day1/measurements.txt";
 
-  // Input should be at least size == 2
+pub fn count_increases() {
+  let measures = read(SAMPLES);
+
   match measures {
     Ok(measures) => {
+      if measures.len() < 2 {
+        println!("The size should be at least 2 ");
+      }
+
       let mut counter = 0;
       for i in 1..measures.len() {
         let res;
@@ -20,6 +25,34 @@ pub fn count_increases() {
 
       println!(
         "Measurements that are larger than the previous measurement: {}",
+        counter
+      );
+    }
+
+    Err(e) => println!("Failed: {:?}", e),
+  }
+}
+
+pub fn sliding_window_increases() {
+  let measures = read(SAMPLES);
+
+  match measures {
+    Ok(measures) => {
+      if measures.len() < 4 {
+        println!("The size should be at least 2 ");
+      }
+
+      let mut counter = 0;
+      for i in 3..measures.len() {
+        let pre_sum = measures[i - 1] + measures[i - 2] + measures[i - 3];
+        let now_sum = measures[i] + measures[i - 1] + measures[i - 2];
+        if pre_sum < now_sum {
+          counter += 1;
+        }
+      }
+
+      println!(
+        "Sums that are larger than the previous sum: {}",
         counter
       );
     }
--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,5 @@ mod day1;
 
 fn main() {
     day1::count_increases();
+    day1::sliding_window_increases();
 }
