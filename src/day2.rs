d3f0dd59bd6b78786bf4278e3ab60684d176125c
Max Bodnar <macsimbodnar@gmail.com>
2021-12-10 19:56:14 +0100

    Day2 part two

--- src/day2.rs
+++ src/day2.rs
@@ -27,3 +27,33 @@ pub fn calculate_position() {
     Err(e) => println!("FailedL {:?}", e),
   }
 }
+
+pub fn calculate_aim() {
+  let commands = read2(SAMPLES);
+
+  let mut x = 0;
+  let mut depth = 0;
+  let mut aim = 0;
+
+  match commands {
+    Ok(commands) => {
+      for i in 0..commands.len() {
+        let (command, value) = &commands[i];
+
+        match command.as_str() {
+          "forward" => {
+            x += value;
+            depth += aim * value;
+          }
+          "down" => aim += value,
+          "up" => aim -= value,
+          _ => println!("Wrong command"),
+        }
+      }
+
+      println!("X: {}    Y: {}    AIM: {}    X*Y: {}", x, depth, aim, x * depth);
+    }
+
+    Err(e) => println!("FailedL {:?}", e),
+  }
+}
